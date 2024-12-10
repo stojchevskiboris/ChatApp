ba9564905ef0dc20b65e6f2c98bed4f57da15d59
Boris Stojchevski <boriss@nextsense.com>
2024-12-10 17:05:09 +0100

    add sp, udt

---
+++ ChatApp.Server/Migrations/20241210133743_CreateUserDefinedTypes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserDefinedTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE TYPE IntListType AS TABLE (
+                    Value INT
+                );
+            ");
+
+            migrationBuilder.Sql(@"
+                CREATE TYPE StringListType AS TABLE (
+                    Value NVARCHAR(MAX)
+                );
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TYPE IF EXISTS StringListType;");
+            migrationBuilder.Sql("DROP TYPE IF EXISTS IntListType;");
+        }
+    }
+}
---
+++ ChatApp.Server/Migrations/20241210134315_AddSearchUsersSp.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSearchUsersSp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Create the stored procedure
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE SearchUsersToAdd
+                    @currentUserId INT,
+                    @terms StringListType READONLY,
+                    @contactIds IntListType READONLY
+                AS
+                BEGIN
+                    SELECT *
+                    FROM Users u
+                    WHERE u.Id != @currentUserId
+                      AND u.Id NOT IN (SELECT Value FROM @contactIds)
+                      AND EXISTS (
+                          SELECT 1
+                          FROM @terms t
+                          WHERE LOWER(u.FirstName) LIKE '%' + t.Value + '%'
+                             OR LOWER(u.LastName) LIKE '%' + t.Value + '%'
+                      );
+                END
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS SearchUsersToAdd;");
+        }
+    }
+}
