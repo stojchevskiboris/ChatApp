315ddd98707bb3ae0a729639e326ee511d3efc2d
Boris Stojchevski <boriss@nextsense.com>
2025-02-17 10:53:23 +0100

    smtp init

---
+++ ChatApp.Server/Common/Helpers/Mail.cs
@@ -0,0 +1,92 @@
+﻿using ChatApp.Server.Common.Exceptions;
+using ChatApp.Server.Services.ViewModels.Common;
+using System.Net.Mail;
+using System.Text;
+
+namespace ChatApp.Server.Common.Helpers
+{
+    public static class Mail
+    {
+        public static void SendMail(string to, string subject, string body, string from = null)
+        {
+            try
+            {
+                var message = new MailMessage
+                {
+                    IsBodyHtml = true,
+                    BodyEncoding = Encoding.UTF8,
+                    HeadersEncoding = Encoding.UTF8,
+                    SubjectEncoding = Encoding.UTF8,
+                    Subject = subject,
+                    Body = body
+                };
+
+                if (!String.IsNullOrEmpty(from))
+                    message.From = new MailAddress(from);
+
+                string[] toArr = to.Split(';');
+                foreach (var item in toArr)
+                {
+                    message.To.Add(item);
+                }
+
+                var client = new SmtpClient();
+                client.UseDefaultCredentials = true;
+                try
+                {
+                    client.Send(message);
+
+                }
+                catch (Exception ex)
+                {
+                    throw new CustomException(ex.Message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
+
+        public static void SendMailWithAtt(string to, string subject, string body, List<EmailWithAttachmentViewModel> attachments, string from)
+        {
+            try
+            {
+                var message = new MailMessage
+                {
+                    IsBodyHtml = true,
+                    BodyEncoding = Encoding.UTF8,
+                    HeadersEncoding = Encoding.UTF8,
+                    SubjectEncoding = Encoding.UTF8,
+                    Subject = subject,
+                    Body = body
+                };
+
+                if (attachments != null)
+                {
+                    foreach (var attachment in attachments)
+                    {
+                        var att = new Attachment(new MemoryStream(attachment.Bytes), attachment.FileName);
+                        message.Attachments.Add(att);
+                    }
+                }
+
+                string[] toArr = to.Split(';');
+                foreach (var item in toArr)
+                {
+                    message.To.Add(item);
+                }
+                message.From = new MailAddress(from);
+                var client = new SmtpClient();
+
+                client.Send(message);
+
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
+    }
+}
---
+++ ChatApp.Server/Services/ViewModels/Common/EmailWithAttachmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Server.Services.ViewModels.Common
+{
+    public class EmailWithAttachmentViewModel
+    {
+        public string FileName { get; set; }
+        public byte[] Bytes { get; set; }
+    }
+}
