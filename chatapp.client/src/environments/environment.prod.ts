2f6a0540396a016fa6c99c5624adefa4271fffc4
Boris Stojchevski <boriss@nextsense.com>
2025-03-07 16:11:18 +0100

    .

--- chatapp.client/src/environments/environment.prod.ts
+++ chatapp.client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-production-api-url.com/api'
+    apiUrl: 'https://your-production-api-url.com/api',
+    signalRUrl: 'https://localhost:7078/chatHub'
   };
